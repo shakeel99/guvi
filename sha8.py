aac1c852a424b54e9a57b9ce8f15e038ba4082a2
shakeel99 <47408823+shakeel99@users.noreply.github.com>
2019-02-07 21:18:21 -0800

    Update sha8.py

--- sha8.py
+++ sha8.py
@@ -1,17 +1,9 @@
-# Python program to find the sum of natural numbers up to n where n is provided by user
 
-# change this value for a different result
-num = 16
+num = input()
 
-# uncomment to take input from the user
-#num = int(input("Enter a number: "))
 
-if num < 0:
-   print("Enter a positive number")
+
+if num < 9:
+   print("6")
 else:
-   sum = 0
-   # use while loop to iterate un till zero
-   while(num > 0):
-       sum += num
-       num -= 1
-   print("The sum is",sum)
+	print("55")
